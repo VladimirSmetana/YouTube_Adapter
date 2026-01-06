bc8ba454cb1ade1d3f3be818d657bba5fc5ab8d0
Сметана Владимир <98716102+VladimirSmetana@users.noreply.github.com>
2026-01-06 12:03:21 +0300

    Update Launch.bat

--- Launch.bat
+++ Launch.bat
@@ -1,4 +1,5 @@
 cd "%USERPROFILE%\Desktop"
 git clone https://github.com/VladimirSmetana/YouTube_Adapter.git
 cd YouTube_Adapter
-start "%USERPROFILE%\Desktop\YouTube_Adapter\YouTube.bat"
+
+call "%USERPROFILE%\Desktop\YouTube_Adapter\YouTube.bat"
