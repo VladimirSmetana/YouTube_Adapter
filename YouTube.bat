bdab101e9650ee59b9a2536d8dbd9a0c53dd8a99
Сметана Владимир <98716102+VladimirSmetana@users.noreply.github.com>
2026-01-06 12:09:12 +0300

    Update YouTube.bat

--- YouTube.bat
+++ YouTube.bat
@@ -1,23 +1,23 @@
+
 setlocal enableextensions
 pushd "%~dp0"
 set PATH=%cd%;%PATH%
 if defined PROCESSOR_ARCHITEW6432 start "" %SystemRoot%\sysnative\cmd.exe /c "%~nx0" %* & goto :EOF
 net session >nul 2>&1 || (
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\GetAdmin.vbs"
-echo UAC.ShellExecute "%~nx0", "%*", "", "runas", 1 >> "%temp%\GetAdmin.vbs"
-cscript //NOLOGO "%temp%\GetAdmin.vbs"
-del /f /q "%temp%\GetAdmin.vbs" >nul 2>&1
-exit
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\GetAdmin.vbs"
+  echo UAC.ShellExecute "%~nx0", "%*", "", "runas", 1 >> "%temp%\GetAdmin.vbs"
+  cscript //NOLOGO "%temp%\GetAdmin.vbs"
+  del /f /q "%temp%\GetAdmin.vbs" >nul 2>&1
+  exit
 )
 
-start "%USERPROFILE%\Desktop\YouTube_Adapter\RMSVC.cmd"
+start "" "%USERPROFILE%\Desktop\YouTube_Adapter\RMSVC.cmd"
 timeout 5
 
-start "%USERPROFILE%\Desktop\YouTube_Adapter\BLUpd.cmd"
+start "" "%USERPROFILE%\Desktop\YouTube_Adapter\BLUpd.cmd"
 timeout 60
 
-
-start "%USERPROFILE%\Desktop\YouTube_Adapter\YouTube.cmd"
+start "" "%USERPROFILE%\Desktop\YouTube_Adapter\YouTube.cmd"
 timeout 5
 
 taskkill /im cmd.exe /f
